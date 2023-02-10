e566df8ac90ad4ab31afd82ae3042bb458ac8774
EmonterrosoF <monterrosoedras.@gmail.com>
2023-02-09 22:40:03 -0600

    add validatin in middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -6,6 +6,11 @@ interface data {
 }
 
 export async function middleware(req: NextRequest) {
+  if (req.method !== "GET")
+    return NextResponse.json(
+      { message: "method not allowed" },
+      { status: 505 }
+    );
   try {
     const slug = req.nextUrl.pathname.split("/").pop();
 
